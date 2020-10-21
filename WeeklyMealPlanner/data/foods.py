e72d2bb5d6e57007cdcbb66324991fac19dd08b9
Samuel Odongo <samieodd@gmail.com>
2020-10-21 15:48:32 +0300

    Simplified enum and created Food class

--- WeeklyMealPlanner/data/foods.py
+++ WeeklyMealPlanner/data/foods.py
@@ -1,15 +1,10 @@
 from enum import Enum
 
-class PrimaryFood(Enum):
-    Rice = 1
-    Chapati = 2
-    Spaghetti = 3
-    Bread = 4
-    Mandazi = 5
+class FoodType(Enum):
+    Breakfast = 1
+    Supper = 2
 
-class SecondaryFood(Enum):
-    Potatoes = 1
-    Ndengu = 2
-    Githeri = 3
-    Mbaazi = 4
-    Eggs = 5
+class Food:
+    name: str
+    type: FoodType
+    price: int
