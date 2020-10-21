e43a0b1c31878cd65dc758574b4ecd6f9b580554
Samuel Odongo <samieodd@gmail.com>
2020-10-21 16:15:30 +0300

    Refactored Meal

--- WeeklyMealPlanner/data/meal.py
+++ WeeklyMealPlanner/data/meal.py
@@ -6,8 +6,10 @@ class Meal:
     secondary_food: Food
     _condiments_price: float = 19.5
 
-    def __init__(self, combo_id: str) -> None:
+    def __init__(self, combo_id: str, primary_food: Food, secondary_food: Food) -> None:
         self.combo_id = combo_id
+        self.primary_food = primary_food
+        self.secondary_food = secondary_food
     
     def meal_price(self) -> float:
         return self.primary_food.price + self.secondary_food.price + self._condiments_price
