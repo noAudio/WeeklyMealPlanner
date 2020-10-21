c8d27627bc7afbc87f00bf7fc8ec7702fde7e77b
Samuel Odongo <samieodd@gmail.com>
2020-10-21 16:15:20 +0300

    Refactored foods

--- WeeklyMealPlanner/data/foods.py
+++ WeeklyMealPlanner/data/foods.py
@@ -6,5 +6,10 @@ class FoodType(Enum):
 
 class Food:
     name: str
-    type: FoodType
+    food_type: FoodType
     price: int
+
+    def __init__(self, name: str, food_type: FoodType, price: int) -> None:
+        self.name = name
+        self.food_type = food_type
+        self.price = price
