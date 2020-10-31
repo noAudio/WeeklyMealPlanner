773a21241e2185f757121b2bf216c8034f6d7735
Samuel Odongo <samieodd@gmail.com>
2020-10-31 03:50:30 +0300

    Pass supper object to template

--- WeeklyMealPlanner/views.py
+++ WeeklyMealPlanner/views.py
@@ -5,6 +5,7 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template
 from WeeklyMealPlanner import app
+from WeeklyMealPlanner.logic.pg import supper1
 
 @app.route('/')
 @app.route('/home')
@@ -14,4 +15,5 @@ def home():
         'index.html',
         title='Home Page',
         year=datetime.now().year,
+        supper=supper1
     )
