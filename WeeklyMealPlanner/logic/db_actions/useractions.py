17bb328529d758cee943746be8c4f3f9528a2fea
Samuel Odongo <samieodd@gmail.com>
2020-12-02 18:10:07 +0300

    add method to list meals over a 7 day period

--- WeeklyMealPlanner/logic/db_actions/useractions.py
+++ WeeklyMealPlanner/logic/db_actions/useractions.py
@@ -2,6 +2,7 @@
 # TODO2: Reroll by deleting a month and randomizing it again
 
 from mealscheduler import MealScheduler
+# from WeeklyMealPlanner.logic.db_actions.mealscheduler import MealScheduler
 from typing import Any, List
 import datetime
 
@@ -12,23 +13,26 @@ class UserActions(MealScheduler):
     table_name: str
     _month: str
     _year: int
+    _date: int
 
     def __init__(self, date: int, month: str, year: int) -> None:
         self._month = month
         self._year = year
+        self._date = date
         self.index = datetime.date(int(year), datetime.datetime.strptime(self._month, '%B').month, int(date)).weekday()
         self.table_name = f'{self._month}_{year}'
 
     def this_week(self) -> List[Any]:
         '''
-        - while loop couonting down to zero to get previous days
-        - while loop counting up to 6 to get the next available days
-        - append all to form a list of numbers
-        - for loop using list of numbers as dates to access the db and create a list of days with meals
-        - return list of days
+        Get a list of meals for the previous day, current day and the next 5 days.
         '''
+        days: List[int] = [self._date - 1, self._date, self._date + 1, self._date + 2, self._date + 3, self._date + 4, self._date + 5]
+        current_meals: List[str] = []
 
-        return []
+        for each_day in days:
+            current_meals.append(self.specified_day_meals(date=each_day, month=self._month, year=self._year))        
+
+        return current_meals
     
     def reroll_month(self) -> str:
         '''
@@ -41,9 +45,3 @@ class UserActions(MealScheduler):
         status = self.randomize_schedule()
         return status
 
-useraction: UserActions = UserActions(1, 'January', 2023)
-print(useraction.reroll_month())
-
-#%%
-
-# %%
